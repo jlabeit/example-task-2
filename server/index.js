13b8e28622d732985fd2eff9b28b6ffe2068ecdc
Julian Labeit <j.labeit@impargo.de>
2019-08-01 16:04:46 +0200

    Add linter to server.

--- server/index.js
+++ server/index.js
@@ -2,16 +2,15 @@ const express = require('express')
 const app = express()
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 const exampleData = require('../data/tracking.json')
 
-
 app.get('/', (req, res) => {
-  // TODO(Task 1): Split tracking data into trip segments for example by using the time property. 
+  // TODO(Task 1): Split tracking data into trip segments for example by using the time property.
   res.send(exampleData)
 })
 
