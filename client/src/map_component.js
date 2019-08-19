4c0272261d8467ce6915ba4dd90154bdade72089
Sherif Elkelesh <sherifelkelesh@bitbucket.com>
2019-08-19 11:19:58 +0200

    Splitting data into segments

--- client/src/map_component.js
+++ client/src/map_component.js
@@ -1,50 +1,62 @@
 /* global fetch, L */
-import React, { useEffect, useRef, useState } from 'react'
-import Moment from 'moment'
+import React, { useEffect, useRef, useState } from 'react';
+import Moment from 'moment';
 
-const getRouteSummary = (locations) => {
-  const to = Moment(locations[0].time).format('hh:mm DD.MM')
-  const from = Moment(locations[locations.length - 1].time).format('hh:mm DD.MM')
-  return `${from} - ${to}`
+const getRouteSummary = locations => {
+  const to = Moment(locations[0].time).format('hh:mm DD.MM');
+  const from = Moment(locations[locations.length - 1].time).format('hh:mm DD.MM');
+  return `${from} - ${to}`;
+};
+
+function colorGen() {
+  var generateColor = Math.floor(Math.random() * 256);
+  return generateColor;
 }
 
 const MapComponent = () => {
-  const map = useRef()
-  const [locations, setLocations] = useState()
+  const map = useRef();
+  const [locations, setLocations] = useState();
   // Request location data.
   useEffect(() => {
     fetch('http://localhost:3000')
       .then(response => response.json())
-      .then((json) => {
-        setLocations(json)
-      })
-  }, [])
+      .then(json => {
+        setLocations(json);
+      });
+  }, []);
   // TODO(Task 2): Request location closest to specified datetime from the back-end.
 
   // Initialize map.
   useEffect(() => {
-    map.current = new L.Map('mapid')
-    const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    const attribution = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+    map.current = new L.Map('mapid');
+    const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const attribution = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
     const osm = new L.TileLayer(osmUrl, {
       minZoom: 8,
       maxZoom: 12,
       attribution
-    })
-    map.current.setView(new L.LatLng(52.51, 13.40), 9)
-    map.current.addLayer(osm)
-  }, [])
+    });
+    map.current.setView(new L.LatLng(52.51, 13.4), 9);
+    map.current.addLayer(osm);
+  }, []);
   // Update location data on map.
   useEffect(() => {
     if (!map.current || !locations) {
-      return // If map or locations not loaded yet.
+      return; // If map or locations not loaded yet.
     }
     // TODO(Task 1): Replace the single red polyline by the different segments on the map.
-    const latlons = locations.map(({ lat, lon }) => [lat, lon])
-    const polyline = L.polyline(latlons, { color: 'red' }).bindPopup(getRouteSummary(locations)).addTo(map.current)
-    map.current.fitBounds(polyline.getBounds())
-    return () => map.current.remove(polyline)
-  }, [locations, map.current])
+    for (let index = 0; index < Object.keys(locations).length; index++) {
+      const routes = locations[Object.keys(locations)[index]];
+      const latlons = routes.map(({ lat, lon }) => [lat, lon]);
+
+      let rgbColor = 'rgb(' + colorGen() + ',' + colorGen() + ',' + colorGen() + ')';
+      const polyline = L.polyline(latlons, { color: rgbColor })
+        .bindPopup(getRouteSummary(routes))
+        .addTo(map.current);
+      map.current.fitBounds(polyline.getBounds());
+    }
+    return () => map.current;
+  }, [locations, map.current]);
   // TODO(Task 2): Display location that the back-end returned on the map as a marker.
 
   return (
@@ -52,7 +64,8 @@ const MapComponent = () => {
       {locations && `${locations.length} locations loaded`}
       {!locations && 'Loading...'}
       <div id='mapid' />
-    </div>)
-}
+    </div>
+  );
+};
 
-export default MapComponent
+export default MapComponent;
--- server/index.js
+++ server/index.js
@@ -1,22 +1,36 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const moment = require('moment');
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-const exampleData = require('../data/tracking.json')
+const exampleData = require('../data/tracking.json');
 
 app.get('/', (req, res) => {
   // TODO(Task 1): Split tracking data into trip segments for example by using the time property.
-  res.send(exampleData)
-})
+  let trackingData = {};
+  for (let index = 0; index < exampleData.length; index++) {
+    const singleData = exampleData[index];
+    let date = moment(singleData.time);
+    if (Object.keys(trackingData).length === 0) trackingData[date] = [singleData];
+    else {
+      let dates = Object.keys(trackingData);
+      let lastDate = moment(dates[dates.length - 1]);
+      if (lastDate.diff(date, 'minutes') > 60) {
+        trackingData[date] = [singleData];
+      } else trackingData[lastDate].push(singleData);
+    }
+  }
+  res.send(trackingData);
+});
 
 app.get('/location/:when', (req, res) => {
   // TODO(Task 2): Return the tracking data closest to `req.params.when` from `exampleData`.
-  res.send({})
-})
+  res.send({});
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
